633c773c5c98e4e6ce0059d8337e60fecdd28673
mbohekale <mbohekale@yahoo.com>
2021-03-24 23:41:20 +0100

    appending files

---
+++ variables/VariableDemo.cpp
@@ -0,0 +1,28 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int yearOfBirth;
+	char gender = 'm';
+	bool isGood = true;
+	float balance = 4.5;
+	double height;
+	cout << "size Of Integer is: " << sizeof(int) <<"  byte"<< endl;
+	cout << "size Of Double is:" << sizeof(double) << "  byte" << endl;
+	cout << "size Of bool is:" << sizeof(bool) << "  byte" << endl;
+	cout << "size Of float is:" << sizeof(float) << "  byte" << endl;
+	cout << "size Of Unsigned int is:" << sizeof(unsigned int) << "  byte" << endl;
+	cout << "min value is:" << INT_MIN << endl;
+	cout << "max value is:" << INT_MAX << endl;
+
+
+	
+
+	system("pause>0");
+}
+
+
