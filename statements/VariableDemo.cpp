d09e938c7e4863b30dfb644f50526d04dbb90cb8
mbohekale <mbohekale@yahoo.com>
2021-03-25 11:09:11 +0100

    appending files

---
+++ statements/VariableDemo.cpp
@@ -0,0 +1,26 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//if the age is greater than or equals to
+	//if the age is less than or equals to
+	//if the age is greater than and  
+	int age;
+	cout << "Please enter your age : "<<endl;
+	cin >> age;
+	if (age>=20) 
+	{
+		cout << "You have entered the correct age"<<endl;
+	}
+	else 
+	{
+		cout << "You have entered the wrong age"<<endl;
+	}
+	return 0;
+}
+
+
