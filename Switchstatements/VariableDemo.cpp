fe41f6725c10b3bf938ec042003799d98ea58309
mbohekale <mbohekale@yahoo.com>
2021-03-25 14:20:01 +0100

    appending files

---
+++ Switchstatements/VariableDemo.cpp
@@ -0,0 +1,30 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//switch statement
+ 
+	char grade;
+	cout << "Please enter the grade" << endl;
+	cin >> grade;
+	switch (grade)
+	{
+	case 'A': cout << "Excellent" << endl;
+		break;
+	case 'B': cout << "Very good" << endl;
+		break;
+	case 'C': cout << "Passed" << endl;
+		break;
+	default:
+		cout<<"Invalid grade"<<endl;
+		break;
+	}
+
+	return 0;
+}
+
+
