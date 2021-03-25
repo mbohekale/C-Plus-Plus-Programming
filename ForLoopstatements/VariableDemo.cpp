3740856ab2e3804779dccfdb8e155b91e4fe23ac
mbohekale <mbohekale@yahoo.com>
2021-03-25 14:39:15 +0100

    appending files

---
+++ ForLoopstatements/VariableDemo.cpp
@@ -0,0 +1,19 @@
+
+
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//For loop statement
+	for (int i = 0; i < 5; i++) 
+	{
+		if(i>=3)
+		cout <<  i << endl;
+	}	
+
+	return 0;
+}
+
+
